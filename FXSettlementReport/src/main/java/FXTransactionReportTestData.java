c819e97f797b26b6fd54dfef9829b5d59e133475
Akhilesh <akhilesh.perla@accenture.com>
2017-04-17 18:26:05 +0100

    Removed the warning messages

--- FXSettlementReport/src/main/java/FXTransactionReportTestData.java
+++ FXSettlementReport/src/main/java/FXTransactionReportTestData.java
@@ -9,11 +9,8 @@
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Currency;
-
 import java.util.List;
-import java.util.Map;
 
-import java.util.stream.Collectors;
 
 public class FXTransactionReportTestData {
 
